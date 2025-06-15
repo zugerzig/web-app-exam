fc1f4a53cbf253479ab5cf8cb8aeb9701c58329a
Leonid Tiulganov <tylgunov712@gmail.com>
2025-06-15 16:44:39 +0300

    app.run(debug=False, host='0.0.0.0')

--- app.py
+++ app.py
@@ -375,4 +375,4 @@ if __name__ == '__main__':
             print('✅ База данных создана')
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
